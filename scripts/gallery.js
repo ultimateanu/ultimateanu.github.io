9b9469fb68a77b4f86bb052212ce45d63cb27094
Anu Bandi <ultimateanu@gmail.com>
2016-01-19 01:23:43 -0800

    make images zoom in from thumbnail

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,27 +1,35 @@
 
 
 var openPhotoSwipe = function () {
+    // TODO: make mscr small version of photos
     // dynamically figure out image properties
     var parseGalleryElements = function (gallery) {
         var items = [];
         for (var i = 0; i < gallery.children.length; i++) {
             var image = gallery.children[i].children[0];
+            var rect = image.getBoundingClientRect();
+            var pageYScroll = window.pageYOffset || document.documentElement.scrollTop;
             var item = {
+                msrc: image.getAttribute('src'),
                 src: image.getAttribute('src'),
                 w: image.naturalWidth,
                 h: image.naturalHeight,
+                offsets: { x: rect.left, y: rect.top + pageYScroll, w: rect.width },
             };
             items.push(item);
         }
         return items;
     };
 
+    var getStartPos = function (index) {
+        return items[index].offsets;
+    }
+    
     var pswpElement = document.querySelectorAll('.pswp')[0];
-    var galleryElement = document.getElementById("acro-gallery");
-    var items = parseGalleryElements(galleryElement);
+    var items = parseGalleryElements(document.getElementById("acro-gallery"));
     var options = {
+        getThumbBoundsFn: getStartPos,
         bgOpacity: 1,
-        showHideOpacity: true,  // fade in image
         preload: [1, 3],        // preload 1 previous img and 3 next
         shareEl: false,         // disable share button
     };
