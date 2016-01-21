b8369a6148ae6808288db8b52bfc38fa98af8399
Anu Bandi <ultimateanu@gmail.com>
2016-01-20 20:26:01 -0800

    added browser-sync to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
 
 // Tasks
 gulp.task('sass', function () {
@@ -8,13 +9,24 @@ gulp.task('sass', function () {
         .pipe(sourcemaps.init())
         .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
         .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./styles'));
+        .pipe(gulp.dest('./styles'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        open: false,
+    });
 });
 
 // Watch tasks
 gulp.task('watch', function () {
     gulp.watch('./styles/**/*.scss', ['sass']);
+    gulp.watch('./**/*.html').on('change', browserSync.reload);
 });
 
 // Default task
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'browser-sync', 'watch']);
